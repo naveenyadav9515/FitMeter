40da66eafbfb3226e2ad832c230a3a696d8ebc41
naveenyadav9515 <naveenyadav9515@gmail.com>
2024-06-15 00:27:58 -0700

    done with basec fake api calls

---
+++ src/app/api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+  public getUsers(): Observable<any[]> {
+    console.log('ended');
+    return this.http.get<any[]>(`http://localhost:3000/users`);
+  }
+}
